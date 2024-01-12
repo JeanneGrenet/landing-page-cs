9a3b30e532baf49829cf1c16ea8efdf3a324d984
Jeanne <jeanne.grenet@gmail.com>
2024-01-12 16:19:43 +0100

    update services section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -13,7 +13,7 @@ export default function HeroSection() {
           <div className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-primary sm:-mr-80 lg:-mr-96" />
           <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
             <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
-              <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-white sm:text-6xl lg:col-span-2 xl:col-auto font-TrashHand">
+              <h1 className="max-w-2xl text-4xl font-bold text-white sm:text-6xl lg:col-span-2 xl:col-auto font-TrashHand tracking-wider">
                 Camel Studio: une boîte de devs agiles et résilients 
               </h1>
               <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
