89c2a17af8468e9b88f79765194f044026561d85
Jeanne <jeanne.grenet@gmail.com>
2024-01-12 16:19:43 +0100

    update services section

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -55,7 +55,7 @@ export default function ServicesSection() {
           <h2 className="text-base font-semibold leading-7 text-secondary">
             Nos prestations
           </h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+          <p className="mt-2 text-3xl font-bold tracking-wider font-TrashHand text-white sm:text-4xl">
             Ce qu’on sait faire ?
           </p>
           <p className="mt-6 text-lg leading-8 text-white">
@@ -69,7 +69,7 @@ export default function ServicesSection() {
           <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
             {features.map((feature) => (
               <div key={feature.name} className="flex flex-col">
-                <dt className="text-base font-semibold leading-7 text-secondary">
+                <dt className="text-xl font-semibold leading-7 text-secondary tracking-wider font-TrashHand">
                   <div className="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-secondary">
                     <Icon
                       src={feature.icon}
