bfcca1781d5e00951ebfc613c9da9871479e5450
Jeanne <jeanne.grenet@gmail.com>
2024-01-15 11:21:21 +0100

    update project section and add presentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import ServicesSection from "../components/services";
 import FooterSection from "@/components/footer";
 import ProjectsSection from "@/components/projects";
 import WorkingSection from "@/components/working";
+import PresentationSection from "@/components/presentation";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <CtaSection label="Prêt à travaillez avec nous ?" />
       <ProjectsSection />
       <WorkingSection />
+      <PresentationSection />
       <CtaSection label="Alors, convaincu ?" />
       <FooterSection />
     </>
